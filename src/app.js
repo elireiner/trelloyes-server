3f19307f8ad752430f6c36163707eb23370046a5
Eli Reiners <goelyukim@gmail.com>
2020-06-14 20:58:41 -0400

    Fix config and refractor

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-require('dotenv').config;
+require('dotenv').config();
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-//winston is a logger
-const winston = require('winston');
+const cardRouter = require('./card/card-router');
+const listRouter = require('./list/list-router');
 
 const app = express()
 
@@ -14,33 +14,19 @@ const morganOption = (NODE_ENV === 'production')
     : 'dev';
 
 app.use(morgan(morganOption))
-app.use(express.json())
 app.use(helmet())
 app.use(cors())
 
-// set up winston
-const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
-    transports: [
-        new winston.transports.File({ filename: 'info.log' })
-    ]
-});
-
-// continue winston set up to log to console when in development
-if (NODE_ENV !== 'production') {
-    logger.add(new winston.transports.Console({
-        format: winston.format.simple()
-    }));
-}
+const lists = [{
+    id: 1,
+    header: 'List One',
+    cardIds: [1]
+}];
 
 app.use(function validateBearerToken(req, res, next) {
 
     const authToken = req.get('Authorization')
     const apiToken = process.env.API_TOKEN;
-    
-
-    console.log(authToken.split(' ')[1], apiToken, authToken.split(' ')[1] !== apiToken)
 
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
         logger.error(`Unauthorized request to path: ${req.path}`);
@@ -50,9 +36,8 @@ app.use(function validateBearerToken(req, res, next) {
     next()
 })
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!')
-})
+app.use(cardRouter);
+app.use(listRouter);
 
 app.use(function errorHandler(error, req, res, next) {
     let response
